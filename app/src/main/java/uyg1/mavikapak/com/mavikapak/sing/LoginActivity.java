e16ecc2e902ed5b72bc552df36072083e768c206
1emre <emredmrbs99@gmail.com>
2018-05-19 23:45:33 +0300

    Allahın izni ile google singin tamamlanlı
    google service ile giriş yapıldı

--- app/src/main/java/uyg1/mavikapak/com/mavikapak/sing/LoginActivity.java
+++ app/src/main/java/uyg1/mavikapak/com/mavikapak/sing/LoginActivity.java
@@ -1,7 +1,12 @@
 package uyg1.mavikapak.com.mavikapak.sing;
 
+import android.app.LoaderManager;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,21 +15,41 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import uyg1.mavikapak.com.mavikapak.R;
 import uyg1.mavikapak.com.mavikapak.ui.UserMainActivity;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener,LoaderManager.LoaderCallbacks<Cursor>,GoogleApiClient.OnConnectionFailedListener {
 
     private FirebaseUser currentUser;
     private FirebaseAuth mAuth;
     private EditText etMail,etSifre;
     private Button btnGiris;
+    // Configure Google Sign In
+    GoogleSignInOptions gso;
+    private SignInButton btnSignIn;
+    private GoogleApiClient mGoogleApiClient;
+    private static final int RC_SIGN_IN = 007;
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +60,54 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         etSifre=findViewById(R.id.etSifre);
         btnGiris=findViewById(R.id.btnGiris);
         btnGiris.setOnClickListener(this);
+        btnSignIn=findViewById(R.id.sign_in_button);
+        btnSignIn.setSize(SignInButton.SIZE_STANDARD);
+        btnSignIn.setOnClickListener(this);
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
 
     }
+
 
     @Override
     protected void onStart() {
         super.onStart();
         currentUser = mAuth.getCurrentUser();
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (opr.isDone()) {
+            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+            // and the GoogleSignInResult will be available instantly. We can try and retrieve an
+            // authentication code.
+
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        } else {
+            // If the user has not previously signed in on this device or the sign-in has expired,
+            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+            // single sign-on will occur in this branch.
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setMessage("Checking sign in state...");
+            progressDialog.show();
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(@NonNull GoogleSignInResult googleSignInResult) {
+                    progressDialog.dismiss();
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
+
     }
 
     @Override
@@ -54,12 +120,93 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 }else {
                     login(etMail.getText().toString().trim(),etSifre.getText().toString().trim());
                 }
-
+            case R.id.sign_in_button:
+                signIn();
                 break;
                 default:break;
 
         }
     }
+
+    private void signIn() {
+        Log.d("sing","startsinging");
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d("sing","startsinging code "+requestCode);
+        if (requestCode == RC_SIGN_IN) {
+            if (requestCode == RC_SIGN_IN) {
+                Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                try {
+                    // Google Sign In was successful, authenticate with Firebase
+                    Log.d("sing","startsinging task "+task.isSuccessful());
+                    GoogleSignInAccount account = task.getResult(ApiException.class);
+                    firebaseAuthWithGoogle(account);
+                } catch (ApiException e) {
+                    // Google Sign In failed, update UI appropriately
+                    Log.w("sign", "Google sign in failed", e);
+                    // ...
+                }
+            }
+
+
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+
+        }
+    }
+    // [START auth_with_google]
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            startActivity(new Intent(getApplicationContext(),UserMainActivity.class));
+
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                        }
+
+
+                    }
+                });
+    }
+// [END auth_with_google]
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d("auth", "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+
+            Log.e("auth", "display name: " + acct.getDisplayName());
+
+            String personName = acct.getDisplayName();
+            //String personPhotoUrl = acct.getPhotoUrl().toString();
+            String email = acct.getEmail();
+
+            Log.e("auth", "Name: " + personName + ", email: " + email
+                    + ", Image: " );
+
+            finish();
+        } else {
+
+        }
+    }
+
 
     private void login(String email, String password) {
         mAuth.signInWithEmailAndPassword(email, password)
@@ -88,5 +235,25 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     public void kaydol(View view) {
 
         startActivity(new Intent(getApplicationContext(),RegisterActivity.class));
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
